71de0419ea36e18b1bd5c6164c13acaf5e2f5eda
ZanyXDEV <23155424+ZanyXDev@users.noreply.github.com>
2020-04-02 17:11:22 +0300

    select level game

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,7 +2,7 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-
+    setupMenu();
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
@@ -13,4 +13,57 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 void MainWindow::keyReleaseEvent(QKeyEvent *event)
 {
     QMainWindow::keyReleaseEvent(event);
+}
+
+void MainWindow::setupMenu()
+{
+    QToolBar *tb = addToolBar(tr("Games toolbar"));
+    QMenu *menu = menuBar()->addMenu(tr("&Game"));
+
+    //const QIcon openIcon = QIcon::fromTheme("document-new", QIcon(rsrcPath + "/fileopen.png"));
+    QAction *a = menu->addAction(tr("&EASY"), this, &MainWindow::newEasy);
+    //a->setShortcut(Qt::Key_E);
+    tb->addAction(a);
+
+    a = menu->addAction(tr("&MEDIUM"), this, &MainWindow::newMedium);
+    //a->setShortcut(Qt::Key_E);
+    tb->addAction(a);
+
+    a = menu->addAction(tr("&HARD"), this, &MainWindow::newHard);
+    //a->setShortcut(Qt::Key_E);
+    tb->addAction(a);
+
+    menu->addSeparator();
+
+    a = menu->addAction(tr("&Quit"), this, &QWidget::close);
+    a->setShortcut(Qt::CTRL + Qt::Key_Q);
+}
+
+
+void MainWindow::newGame()
+{
+
+}
+
+void MainWindow::newEasy()
+{
+     m_level = LevelTypes::EASY;
+     m_size = 8;
+     m_mines = 10;
+     newGame();
+}
+
+void MainWindow::newMedium()
+{
+     m_level = LevelTypes::MEDIUM;
+     m_size = 8;
+     m_mines = 10;
+     newGame();
+}
+void MainWindow::newHard()
+{
+     m_level = LevelTypes::HARD;
+     m_size = 8;
+     m_mines = 10;
+     newGame();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,21 +3,45 @@
 
 #include <QMainWindow>
 #include <QKeyEvent>
+#include <QToolBar>
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+#include <QDebug>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    Q_ENUMS(LevelTypes)
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-
+    enum LevelTypes{
+        EASY,
+        MEDIUM,
+        HARD
+    };
 signals:
 
-public slots:
+public slots:  
+
+private slots:
+    void newEasy();
+    void newMedium();
+    void newHard();
 
 protected:
     void keyPressEvent(QKeyEvent *event);
     void keyReleaseEvent(QKeyEvent *event);
+
 private:
+    void newGame();
+    void setupMenu();
+
+    LevelTypes m_level;
+    quint8 m_size;
+    quint8 m_mines;
 };
+
+
 
 #endif // MAINWINDOW_H

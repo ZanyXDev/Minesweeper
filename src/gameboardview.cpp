55d7322e5e7710e34271ee194e602fab23381a39
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2020-10-12 13:51:26 +0300

    Find clicked item and send signal

--- src/gameboardview.cpp
+++ src/gameboardview.cpp
@@ -8,12 +8,21 @@ GameBoardView::GameBoardView(QGraphicsView *parent)
 
 void GameBoardView::mousePressEvent(QMouseEvent *event)
 {
-    qDebug() << "Custom view clicked at :" << event->pos();
-    if(event->button() == Qt::LeftButton) {
-         qDebug() << "Left button click";
+    if (QGraphicsItem *item = itemAt(event->pos())) {
+        qDebug() << "item type:" << item->type();
+        TileItem *cell = qgraphicsitem_cast<TileItem *>( item );
+        if ( cell ){
+//            qDebug() << "You clicked on TileItem" <<  cell
+//                     << " at :" << event->pos()
+//                     << " position:" << cell->getPosition();
+            if(event->button() == Qt::LeftButton) {
+                emit itemClicked( cell->getPosition(), 0 );
+            }
+            if(event->button() == Qt::RightButton) {
+                 emit itemClicked( cell->getPosition(), 1 );
+            }
+        }
     }
-    if(event->button() == Qt::RightButton) {
-         qDebug() << "Right button click";
-    }
+
     QGraphicsView::mousePressEvent(event);
 }
--- src/gameboardview.h
+++ src/gameboardview.h
@@ -4,6 +4,7 @@
 #include <QGraphicsView>
 #include <QDebug>
 #include <QMouseEvent>
+#include "tileitem.h"
 
 class GameBoardView : public QGraphicsView
 {
@@ -12,7 +13,12 @@ public:
     explicit GameBoardView(QGraphicsView *parent = nullptr);
 
 signals:
-
+    /**
+     * @brief itemClicked user click in the elemenet item, need recalc game matrix
+     * @param index Position cliked element in the matrix
+     * @param mouseButton 0 click LeftButton 1 click RightButton
+     */
+    void itemClicked(int index, int mouseButton );
 public slots:
 
 protected:
